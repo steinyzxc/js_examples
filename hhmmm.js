c465be9e3087d1e7fb4fd82954b9a432d83aac8a
yabloko <50886942+y4610k0@users.noreply.github.com>
2023-09-20 20:32:30 +0300

    hhmmm

--- hhmmm.js
+++ hhmmm.js
@@ -6,5 +6,5 @@ let head = document.getElementsByTagName('head')[0];
 let link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = 'https://github.com/steinyzxc/js_examples/bebra.css';
+link.href = 'https://raw.githubusercontent.com/steinyzxc/js_examples/main/bebra.css';
 head.appendChild(link);
