d7e9e6fca8b1100cad1e524de73dfa3e8873f1d1
yabloko <50886942+y4610k0@users.noreply.github.com>
2023-09-20 20:33:59 +0300

    a

--- hhhmmm.js
+++ hhhmmm.js
@@ -5,6 +5,6 @@ if (elementToRemove) {
 let head = document.getElementsByTagName('head')[0];
 let link = document.createElement('link');
 link.rel = 'stylesheet';
-link.type = 'text/css';
+
 link.href = 'https://raw.githubusercontent.com/steinyzxc/js_examples/main/bebra.css';
 head.appendChild(link);
